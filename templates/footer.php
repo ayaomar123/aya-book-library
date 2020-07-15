41dbb80c51be676d24674d03194a0636dd9e432a
ayaomar123 <44114766+ayaomar123@users.noreply.github.com>
2020-07-15 19:34:15 +0300

    first-commit

---
+++ templates/footer.php
@@ -0,0 +1,14 @@
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   text-align: center;
+}
+</style>
+
+  <footer class="footer center">
+  	<p class="m-0">All copyright reserved to <a href="index.php">Aya Al Rahman</a> 2020</p>
+  </footer>
+  </body>
